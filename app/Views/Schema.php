f46fab538f0a3e8310af0b3af1e8042ffd136b3a
Martins Eglitis <sitilge@gmail.com>
2016-04-28 01:54:09 +0300

    Updated schema view

--- app/Views/Schema.php
+++ app/Views/Schema.php
@@ -4,6 +4,6 @@
 <blockquote>
     <p>curdle - separate or cause to separate into curds or lumps</p>
 </blockquote>
-<p>curdle is a simple database administration tool for simple <a href="https://en.wikipedia.org/wiki/Create,_read,_update_and_delete">CRUD</a> operations using almost any relational database you prefer.</p>
-<h2 id="demo">Features, Tech, Configuration</h2>
-<p>Please see official the github page here: <a href="https://github.com/sitilge/curdle">https://github.com/sitilge/curdle</a></p>
+<p>curdle is a simple database administration tool for simple <a href="https://en.wikipedia.org/wiki/Create,_read,_update_and_delete">CRUD</a> operations.</p>
+<h2 id="demo">Features, Configuration</h2>
+<p>Please see the github page here: <a href="https://github.com/sitilge/curdle">https://github.com/sitilge/curdle</a></p>
