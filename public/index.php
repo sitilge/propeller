959ac669759bf8d22b262fd9869956c7fdcb7b48
Martins Eglitis <sitilge@gmail.com>
2016-07-12 15:35:44 +0300

    Removed dd function

--- public/index.php
+++ public/index.php
@@ -1,15 +1,5 @@
 <?php
 
-//TODO - delete this
-function dd($i)
-{
-    ob_clean();
-    echo '<pre>';
-    var_dump($i);
-    echo '<pre>';
-    exit;
-}
-
 require __DIR__.'/../vendor/autoload.php';
 
 $frontController = new \Propeller\Misc\Bootstrap();
